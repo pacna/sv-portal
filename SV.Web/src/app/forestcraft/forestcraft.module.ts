9b63f7df563a36980fc0e2a8e72a55b17433dab8
pacna <31336845+pacna@users.noreply.github.com>
2022-04-30 00:57:05 -0400

    structure import

--- SV.Web/src/app/forestcraft/forestcraft.module.ts
+++ SV.Web/src/app/forestcraft/forestcraft.module.ts
@@ -1,15 +1,22 @@
-import { MatIconModule } from '@angular/material/icon';
-import { SharedModule } from './../shared/shared.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+// Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+
+// Shared
+import { SharedModule } from './../shared/shared.module';
+import { CardManagementModule } from '../shared/modules/card-management';
+
+// Self
 import { forestCraftRoutes } from './forestcraft.routing';
 import { ForestcraftOverviewComponent } from './components/forestcraft-overview/forestcraft-overview.component';
 import { ForestcraftDetailComponent } from './components/forestcraft-detail/forestcraft-detail.component';
-import { CardManagementModule } from '../shared/modules/card-management';
-import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [ForestcraftOverviewComponent, ForestcraftDetailComponent],
