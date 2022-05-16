b8bc5cc3376f0854b600c024d2a198aec2eca284
pacna <31336845+pacna@users.noreply.github.com>
2022-05-16 01:01:59 -0400

    use localhost

--- SV.Edge/src/SV.Edge/Sql/load-db.sh
+++ SV.Edge/src/SV.Edge/Sql/load-db.sh
@@ -1,7 +1,7 @@
 # use for dev and demo purposes
 export PGUSER=test
 export PGPASSWORD=test
-export PGHOST=127.0.0.1
+export PGHOST=localhost
 export PGDB=sv_portal
 
 psql -U $PGUSER -h $PGHOST -d $PGDB -f schema-down.sql
