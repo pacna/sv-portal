7c291eee6f8c2a25c3ee20447078b0be0b0da97d
pacna <31336845+pacna@users.noreply.github.com>
2023-08-12 03:28:33 -0400

    fix unneeded mocks

--- SV.Web/src/app/shared/testing/shared-mocks.spec.ts
+++ SV.Web/src/app/shared/testing/shared-mocks.spec.ts
@@ -7,17 +7,6 @@ import { CardDetailResponse } from './../types/api/card-detail-response';
 import { PageSuccessState } from '../types/customs/page-success-state.enum';
 
 // https://stackoverflow.com/questions/41240163/mocking-child-components-angular-2
-@Component({
-  selector: 'top-nav',
-  template: '',
-})
-export class MockTopNavComponent {}
-
-@Component({
-  selector: 'sv-layout',
-  template: '',
-})
-export class MockSVLayoutComponent {}
 
 @Component({
   selector: 'follower-description',
